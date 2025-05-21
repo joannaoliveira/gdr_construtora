544de1c1bbb506456518fad838f9495fcbc07445
Joanna Oliveira <joannaoliveira44#gmail.com>
2025-05-21 15:57:23 -0300

    Header

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,2 +1,7 @@
 $( document ).ready(function() {
+
+    $(".headerMain__menu--action").click(function(){
+        ($("#header").hasClass("mainNav")) ? $("#header").removeClass("mainNav") : $("#header").addClass("mainNav");
+    });
+    
 });
