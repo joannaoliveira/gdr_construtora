c4eee075974cd24c21dab3943bd86e698ec71bca
Joanna Oliveira <joannaoliveira44@gmail.com>
2025-05-25 15:02:58 -0300

    Retrofit

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,35 @@
 $( document ).ready(function() {
 
+    $('.beforeAfter').beforeAfter();
+
     $(".headerMain__menu--action").click(function(){
         ($("#header").hasClass("mainNav")) ? $("#header").removeClass("mainNav") : $("#header").addClass("mainNav");
     });
     
+});
+
+$('.beforeAfter').beforeAfter({
+
+  // is draggable/swipeable
+  movable: true,
+
+  // click image to move the slider
+  clickMove: true,
+
+  // always show the slider
+  alwaysShow: true,
+
+  // intial position of the slider
+  position: 50,
+
+  // opacity between 0 and 1
+  opacity: 0.4,
+  activeOpacity: 1,
+  hoverOpacity: 0.8,
+
+  // slider colors
+  separatorColor: '#ffffff',
+  bulletColor: '#ffffff',
+  arrowColor: '#333333',
+  
 });
